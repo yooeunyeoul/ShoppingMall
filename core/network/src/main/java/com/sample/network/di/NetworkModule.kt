da244e44288d549775ef46f9ec02ec2a626dfc33
youeuneyol <ey.yoo@fanmaum.com>
2023-05-19 18:17:39 +0900

    edit: baseUrl 설정, networkModule di 수정
    - baseUrl 추가
    - di에 불필요한 @Named() 상태값 삭제

--- core/network/src/main/java/com/sample/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/sample/network/di/NetworkModule.kt
@@ -10,17 +10,17 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Named
 import javax.inject.Singleton
+
+private const val baseUrl = "https://media.29cm.co.kr/codingtest/"
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     @Provides
-    @Named("httpClient")
     @Singleton
-    fun provideHttpClient(@Named("interceptor") interceptor: Interceptor): OkHttpClient {
+    fun provideHttpClient(interceptor: Interceptor): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(interceptor)
             .addInterceptor(HttpLoggingInterceptor().apply {
@@ -32,9 +32,7 @@ object NetworkModule {
     }
 
 
-
     @Provides
-    @Named("interceptor")
     @Singleton
     fun provideInterceptor(): Interceptor {
         return Interceptor { chain ->
@@ -48,11 +46,10 @@ object NetworkModule {
 
 
     @Provides
-    @Named("retrofit")
     @Singleton
-    fun provideRetrofitInstance(@Named("httpClient") okHttpClient: OkHttpClient): Retrofit {
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(baseUrl)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
