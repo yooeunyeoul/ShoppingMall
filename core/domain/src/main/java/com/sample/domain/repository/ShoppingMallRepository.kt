542a8052a69b92a314c119dd87ec08f1b21a186c
youeuneyol <ey.yoo@fanmaum.com>
2023-05-19 21:19:00 +0900

    add: domain layer repository 정의

---
+++ core/domain/src/main/java/com/sample/domain/repository/ShoppingMallRepository.kt
@@ -0,0 +1,13 @@
+package com.sample.domain.repository
+
+import androidx.paging.PagingData
+import com.sample.domain.model.Banner
+import com.sample.domain.model.Category
+import com.sample.domain.model.Feed
+import kotlinx.coroutines.flow.Flow
+
+interface ShoppingMallRepository {
+    fun getBannerList(): Flow<List<Banner>>
+    fun getProductList(): Flow<PagingData<Category>>
+    fun getFeedList(): Flow<PagingData<Feed>>
+}
