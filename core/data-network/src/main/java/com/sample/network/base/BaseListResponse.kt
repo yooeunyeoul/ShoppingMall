2370d714bce3299e00443c4ed47712a8615e62ef
youeuneyol <ey.yoo@fanmaum.com>
2023-05-20 14:45:29 +0900

    edit: BaseListResponse Any 타입 삭제

--- core/data-network/src/main/java/com/sample/network/base/BaseListResponse.kt
+++ core/data-network/src/main/java/com/sample/network/base/BaseListResponse.kt
@@ -1,7 +1,7 @@
 package com.sample.network.base
 
-data class BaseListResponse<T : Any>(
-    val error_code: Any,
+data class BaseListResponse<T>(
+    val error_code: Any?,
     val list: List<T>,
     val result: String
 )
