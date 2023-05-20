3bf19ad318b5c176552283f7477982e2d1cd68c0
youeuneyol <ey.yoo@fanmaum.com>
2023-05-20 14:46:25 +0900

    add:  Banner 리스트 가져오기 Dao function 추가

--- core/data-localdata/src/main/java/com/sample/localdata/local/ImageDao.kt
+++ core/data-localdata/src/main/java/com/sample/localdata/local/ImageDao.kt
@@ -10,17 +10,16 @@ import kotlinx.coroutines.flow.Flow
 interface ImageDao {
 
     @Upsert
-    suspend fun upsertAll(images: List<SampleEntity>)
+    suspend fun upsertAll(images: List<BannerEntity>)
 
-    @Query("SELECT * FROM image_entity")
-    fun pagingSource(): PagingSource<Int, SampleEntity>
+    @Query("SELECT * FROM banner_entity")
+    fun pagingSource(): PagingSource<Int, BannerEntity>
 
-    @Query("DELETE FROM image_entity")
+    @Query("DELETE FROM banner_entity")
     suspend fun clearAll()
 
-    @Query("SELECT * FROM image_entity WHERE isFavorite = 1")
-    fun getFavoriteImages(): Flow<List<SampleEntity>>
 
-    @Query("UPDATE image_entity SET isFavorite =:isFavorite WHERE id=:id ")
-    suspend fun imageLikeUpdate(id: String, isFavorite: Boolean)
+    @Query("SELECT * FROM banner_entity")
+    fun getBannerList(): Flow<List<BannerEntity>>
+
 }
