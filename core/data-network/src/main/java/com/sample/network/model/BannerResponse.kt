678a181f2a20864cb5d580309ec887074f737a63
youeuneyol <ey.yoo@fanmaum.com>
2023-05-20 23:13:54 +0900

    delete: 참조 없는 파일 삭제
    - BannerResponse.kt
    - CategoryResponse.kt
    - FeedResponse.kt

--- core/data-network/src/main/java/com/sample/network/model/BannerResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.sample.network.model
-
-import com.sample.network.base.BaseListResponse
-
-data class BannerResponse(
-    val baseListResponse: BaseListResponse<BannerDto>
-)
--- core/data-network/src/main/java/com/sample/network/model/CategoryResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.sample.network.model
-
-import com.sample.network.base.BaseListResponse
-
-data class CategoryResponse(
-    val baseListResponse: BaseListResponse<CategoryDto>,
-    val total_count: Int
-)
