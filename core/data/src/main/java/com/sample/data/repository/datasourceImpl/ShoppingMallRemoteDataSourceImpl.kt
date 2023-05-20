8a166c57d6e35af6c03955623519562b81608aeb
youeuneyol <ey.yoo@fanmaum.com>
2023-05-20 23:34:40 +0900

    add: banner api 호출 비지니스 로직 추가
    -

--- core/data/src/main/java/com/sample/data/repository/datasourceImpl/ShoppingMallRemoteDataSourceImpl.kt
+++ core/data/src/main/java/com/sample/data/repository/datasourceImpl/ShoppingMallRemoteDataSourceImpl.kt
@@ -1,40 +1,45 @@
 package com.sample.data.repository.datasourceImpl
 
+import android.util.Log
 import androidx.paging.PagingData
 import com.sample.data.mappers.toBanner
 import com.sample.data.repository.datasource.ShoppingMallRemoteDataSource
 import com.sample.domain.model.Banner
 import com.sample.domain.model.Feed
+import com.sample.domain.response.NetworkResult
+import com.sample.domain.util.Resource
 import com.sample.localdata.local.ShoppingMallDatabase
 import com.sample.network.remote.ShoppingApi
-import com.sample.network.response.NetworkResult
 import com.sample.network.response.toNetworkResult
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import java.lang.Exception
 
 class ShoppingMallRemoteDataSourceImpl(
     private val api: ShoppingApi,
     private val db: ShoppingMallDatabase
 ) : ShoppingMallRemoteDataSource {
-    override suspend fun getBannerResponse(): Flow<NetworkResult<List<Banner>>> {
-        val networkResponse = api.getBannerList().baseListResponse.toNetworkResult()
-        return when (networkResponse) {
-            is NetworkResult.Success -> {
-                flow { NetworkResult.Success(data = networkResponse.data.map { it.toBanner() }) }
-            }
-
-            is NetworkResult.Fail -> {
-                flow {
-                    NetworkResult.Fail(
-                        data = db.imageDao().getBannerList().map { it.map { it.toBanner() } },
-                        networkResponse.message
-                    )
+    override fun getBannerResponse(): Flow<Resource<List<Banner>>> {
+        return flow {
+            try {
+                when (val networkResponse = api.getBannerList().toNetworkResult()) {
+                    is NetworkResult.Success -> {
+                        emit(Resource.Success(data = networkResponse.data.map { it.toBanner() }))
+                    }
+                    is NetworkResult.Fail -> {
+                        val bannerEntityList = db.imageDao().getBannerList().first()
+                        val bannerList = bannerEntityList.map { it.toBanner() }
+                        emit(Resource.Error(message = networkResponse.message ?: "", data = bannerList))
+                    }
                 }
+            } catch (e: Exception) {
+                val bannerEntityList = db.imageDao().getBannerList().first()
+                val bannerList = bannerEntityList.map { it.toBanner() }
+                emit(Resource.Error(message = e.message ?: "", data = bannerList))
             }
         }
     }
-
 
     override fun getCategoryList(): Flow<PagingData<List<Banner>>> {
         TODO("Not yet implemented")
