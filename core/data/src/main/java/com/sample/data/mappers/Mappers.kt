d505caf827e5c18d428cf2a19c0ad6186bf7dd92
youeuneyol <ey.yoo@fanmaum.com>
2023-05-22 18:47:05 +0900

    add: mapper toCategory 추가
    - DataLayer DB 에서 DomainLayer 로 레이어간의 이동을 위해 데이터를 매퍼로 변경 해주기위함

--- core/data/src/main/java/com/sample/data/mappers/Mappers.kt
+++ core/data/src/main/java/com/sample/data/mappers/Mappers.kt
@@ -1,8 +1,12 @@
 package com.sample.data.mappers
 
 import com.sample.domain.model.Banner
+import com.sample.domain.model.Category
+import com.sample.domain.model.SaleInfo
 import com.sample.localdata.local.BannerEntity
+import com.sample.localdata.local.CategoryEntity
 import com.sample.network.model.BannerDto
+import com.sample.network.model.CategoryDto
 
 fun BannerDto.toBanner(): Banner {
     return Banner(bannerNo = banner_no, imageUrl = image_url)
@@ -10,4 +14,17 @@ fun BannerDto.toBanner(): Banner {
 
 fun BannerEntity.toBanner(): Banner {
     return Banner(bannerNo = bannerNo, imageUrl = imageUrl)
+}
+
+fun CategoryEntity.toCategory(): Category {
+    return Category(
+        brandName = brandName,
+        heartCount = heartCount,
+        imageUrl = imageUrl,
+        itemName = itemName,
+        itemNo = itemNo,
+        reviewAveragePoint = reviewAveragePoint,
+        reviewCount = reviewCount,
+        saleInfo = SaleInfo()
+    )
 }
