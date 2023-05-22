c2f307c9eb60a7d85cedf207bf3df82a2f09f796
youeuneyol <ey.yoo@fanmaum.com>
2023-05-22 18:59:58 +0900

    edit: categoryType 파라미터 추가

--- core/data/src/main/java/com/sample/data/repository/datasource/ShoppingMallRemoteDataSource.kt
+++ core/data/src/main/java/com/sample/data/repository/datasource/ShoppingMallRemoteDataSource.kt
@@ -2,12 +2,14 @@ package com.sample.data.repository.datasource
 
 import androidx.paging.PagingData
 import com.sample.domain.model.Banner
+import com.sample.domain.model.Category
 import com.sample.domain.model.Feed
+import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface ShoppingMallRemoteDataSource {
     fun getBannerResponse(): Flow<Resource<List<Banner>>>
-    fun getCategoryList(): Flow<PagingData<List<Banner>>>
-    fun getFeedList(): Flow<PagingData<List<Feed>>>
+    fun getCategoryList(categoryType: CategoryType): Flow<PagingData<Category>>
+    fun getFeedList(): Flow<PagingData<Feed>>
 }
