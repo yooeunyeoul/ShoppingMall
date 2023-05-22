d0efdc2c0d220ee9c59d62930a315fdb08167609
youeuneyol <ey.yoo@fanmaum.com>
2023-05-22 18:39:16 +0900

    edit: getCategoryList 파라미터 수정
    - 타입별로 카테고리 리스트 가져오기위해 추가

--- core/domain/src/main/java/com/sample/domain/repository/ShoppingMallRepository.kt
+++ core/domain/src/main/java/com/sample/domain/repository/ShoppingMallRepository.kt
@@ -4,11 +4,12 @@ import androidx.paging.PagingData
 import com.sample.domain.model.Banner
 import com.sample.domain.model.Category
 import com.sample.domain.model.Feed
+import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface ShoppingMallRepository {
     fun getBannerList(): Flow<Resource<List<Banner>>>
-    fun getCategoryList(): Flow<PagingData<Category>>
+    fun getCategoryList(categoryType: CategoryType): Flow<PagingData<Category>>
     fun getFeedList(): Flow<PagingData<Feed>>
 }
