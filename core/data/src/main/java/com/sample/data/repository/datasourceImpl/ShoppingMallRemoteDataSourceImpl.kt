61056f5dcf11778eb925c81d1d433c5d3e428752
youeuneyol <ey.yoo@fanmaum.com>
2023-05-22 19:00:47 +0900

    add: paging data를 반환해주는 Pager 정의

--- core/data/src/main/java/com/sample/data/repository/datasourceImpl/ShoppingMallRemoteDataSourceImpl.kt
+++ core/data/src/main/java/com/sample/data/repository/datasourceImpl/ShoppingMallRemoteDataSourceImpl.kt
@@ -1,12 +1,23 @@
+@file:OptIn(ExperimentalPagingApi::class)
+
 package com.sample.data.repository.datasourceImpl
 
-import android.util.Log
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.map
+import com.sample.data.mappers.toCategory
+import com.sample.localdata.local.CategoryEntity
+import kotlinx.coroutines.flow.map
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.sample.data.mappers.toBanner
 import com.sample.data.repository.datasource.ShoppingMallRemoteDataSource
+import com.sample.data_paging.CategoryListRemoteMediator
 import com.sample.domain.model.Banner
+import com.sample.domain.model.Category
 import com.sample.domain.model.Feed
 import com.sample.domain.response.NetworkResult
+import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
 import com.sample.localdata.local.ShoppingMallDatabase
 import com.sample.network.remote.ShoppingApi
@@ -14,7 +25,6 @@ import com.sample.network.response.toNetworkResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import java.lang.Exception
 
 class ShoppingMallRemoteDataSourceImpl(
     private val api: ShoppingApi,
@@ -27,10 +37,16 @@ class ShoppingMallRemoteDataSourceImpl(
                     is NetworkResult.Success -> {
                         emit(Resource.Success(data = networkResponse.data.map { it.toBanner() }))
                     }
+
                     is NetworkResult.Fail -> {
                         val bannerEntityList = db.imageDao().getBannerList().first()
                         val bannerList = bannerEntityList.map { it.toBanner() }
-                        emit(Resource.Error(message = networkResponse.message ?: "", data = bannerList))
+                        emit(
+                            Resource.Error(
+                                message = networkResponse.message ?: "",
+                                data = bannerList
+                            )
+                        )
                     }
                 }
             } catch (e: Exception) {
@@ -41,11 +57,23 @@ class ShoppingMallRemoteDataSourceImpl(
         }
     }
 
-    override fun getCategoryList(): Flow<PagingData<List<Banner>>> {
-        TODO("Not yet implemented")
+    @OptIn(ExperimentalPagingApi::class)
+    override fun getCategoryList(categoryType: CategoryType): Flow<PagingData<Category>> {
+        val pagingSourceFactory = { db.imageDao().pagingSource() }
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            remoteMediator = CategoryListRemoteMediator(
+                api = api,
+                db = db,
+                categoryType = categoryType
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow.map { value: PagingData<CategoryEntity> ->
+            value.map { categoryEntity -> categoryEntity.toCategory() }
+        }
     }
 
-    override fun getFeedList(): Flow<PagingData<List<Feed>>> {
+    override fun getFeedList(): Flow<PagingData<Feed>> {
         TODO("Not yet implemented")
     }
 
