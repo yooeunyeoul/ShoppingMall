da433acdd682c34899d7cc95d4e993251c9f58d6
youeuneyol <ey.yoo@fanmaum.com>
2023-05-22 19:02:45 +0900

    edit: HomeViewModel 카테고리 리스트 가져오는 로직 추가

--- app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeViewModel.kt
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeViewModel.kt
@@ -2,16 +2,24 @@ package com.sample.shoppingmall.presentation.shoppingHome
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.sample.domain.usecase.GetBannerListUseCase
+import com.sample.domain.usecase.GetCategoryListUseCase
+import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    bannerUseCase: GetBannerListUseCase
+    bannerUseCase: GetBannerListUseCase,
+    categoryUseCase: GetCategoryListUseCase,
+
 ) : ViewModel() {
 
     val bannerList = bannerUseCase().stateIn(
@@ -19,5 +27,33 @@ class HomeViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = Resource.Loading()
     )
+
+    val womenList =
+        categoryUseCase(categoryType = CategoryType.WOMEN).flowOn(Dispatchers.IO)
+            .cachedIn(viewModelScope).stateIn(
+                viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = PagingData.empty()
+            )
+
+    val menList =
+        categoryUseCase(categoryType = CategoryType.MEN).flowOn(Dispatchers.IO).cachedIn(viewModelScope).stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PagingData.empty()
+        )
+    val techList =
+        categoryUseCase(categoryType = CategoryType.TECH).flowOn(Dispatchers.IO).cachedIn(viewModelScope).stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PagingData.empty()
+        )
+
+    val homeList =
+        categoryUseCase(categoryType = CategoryType.HOME).flowOn(Dispatchers.IO).cachedIn(viewModelScope).stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PagingData.empty()
+        )
 
 }
