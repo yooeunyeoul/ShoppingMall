71f02321d932e1dd05b0aed00156eca4140ce338
youeuneyol <ey.yoo@fanmaum.com>
2023-05-22 19:02:16 +0900

    edit: HomeScreen UI 구현

--- app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeScreen.kt
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeScreen.kt
@@ -1,90 +1,190 @@
-@file:OptIn(ExperimentalFoundationApi::class, ExperimentalPagerApi::class)
+@file:OptIn(ExperimentalPagerApi::class)
 
 package com.sample.shoppingmall.presentation.shoppingHome
 
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import com.sample.domain.model.Category
 import com.sample.domain.util.Resource
-import com.sample.shoppingmall.R
+import com.sample.shoppingmall.presentation.util.HomeTabType
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
     val bannerState by viewModel.bannerList.collectAsStateWithLifecycle()
-    var selectedIndex by remember {
-        mutableStateOf(0)
-    }
+    val womenList = viewModel.womenList.collectAsLazyPagingItems()
+    val menList = viewModel.menList.collectAsLazyPagingItems()
+    val homeList = viewModel.homeList.collectAsLazyPagingItems()
+    val techList = viewModel.techList.collectAsLazyPagingItems()
 
+    BoxWithConstraints {
+        val screenHeight = maxHeight
+        val scrollState = rememberScrollState()
 
-    LazyColumn(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        item {
-            when (bannerState) {
-                is Resource.Error -> {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(state = scrollState)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.LightGray), contentAlignment = Alignment.Center
+            ) {
+                when (bannerState) {
+                    is Resource.Error -> {
 
+                    }
+
+                    is Resource.Loading -> {
+                        CircularProgressIndicator()
+                    }
+
+                    is Resource.Success -> {
+                        HorizontalPager(count = bannerState.data?.count() ?: 0) { page ->
+                            AsyncImage(
+                                model = ImageRequest.Builder(LocalContext.current)
+                                    .data(bannerState.data?.get(page)?.imageUrl)
+//                                    .data(R.drawable.sample)
+                                    .crossfade(true).build(),
+                                contentScale = ContentScale.FillWidth,
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
                 }
 
-                is Resource.Loading -> {
-                    CircularProgressIndicator()
-                }
+            }
 
-                is Resource.Success -> {
-                    HorizontalPager(count = bannerState.data?.count() ?: 0) { page ->
-                        AsyncImage(
-                            model = ImageRequest.Builder(LocalContext.current)
-//                            .data(bannerState.data?.get(page)?.imageUrl)
-                                .data(R.drawable.sample)
-                                .crossfade(true).build(),
-                            contentScale = ContentScale.FillWidth,
-                            contentDescription = null,
-                            modifier = Modifier.fillMaxWidth()
-                        )
+            Column(modifier = Modifier.height(screenHeight)) {
+                val pagerState = rememberPagerState(initialPage = 0)
+                val coroutineScope = rememberCoroutineScope()
+
+                TabRow(
+                    selectedTabIndex = pagerState.currentPage,
+                    indicator = {},
+                    modifier = Modifier.wrapContentSize(),
+                    tabs = {
+                        HomeTabType.values().forEachIndexed { index, categoryType ->
+                            Tab(selected = pagerState.currentPage == index, onClick = {
+                                coroutineScope.launch {
+                                    pagerState.animateScrollToPage(index)
+                                }
+                            }) {
+                                Text(
+                                    text = categoryType.name,
+                                    color = if (pagerState.currentPage == index) Color.Black else Color.LightGray
+                                )
+                            }
+                        }
+                    })
+                HorizontalPager(
+                    state = pagerState,
+                    count = HomeTabType.values().size,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .nestedScroll(remember {
+                            object : NestedScrollConnection {
+                                override fun onPreScroll(
+                                    available: Offset,
+                                    source: NestedScrollSource
+                                ): Offset {
+                                    return if (available.y > 0) Offset.Zero else Offset(
+                                        x = 0f,
+                                        y = -scrollState.dispatchRawDelta(-available.y)
+                                    )
+                                }
+                            }
+                        })
+                ) { page: Int ->
+                    when (page) {
+                        0 -> {
+                            ListLazyColumn(menList)
+                        }
+
+                        1 -> {
+                            ListLazyColumn(womenList)
+                        }
+
+                        2 -> {
+                            ListLazyColumn(homeList)
+                        }
+
+                        3 -> {
+                            ListLazyColumn(techList)
+                        }
+
+                        4 -> {
+//                            ListLazyColumn(menList)
+                        }
                     }
                 }
             }
         }
-        stickyHeader {
-            TabRow(selectedTabIndex = selectedIndex, modifier = Modifier.height(48.dp)) {
-                Tab(selected = selectedIndex == 0, onClick = { selectedIndex = 0 }) {
-                    Text(text = "WOMEN")
-                }
-                Tab(selected = selectedIndex == 1, onClick = { selectedIndex = 1 }) {
-                    Text(text = "MEN")
-                }
-                Tab(selected = selectedIndex == 2, onClick = { selectedIndex = 2 }) {
-                    Text(text = "HOME")
-                }
-                Tab(selected = selectedIndex == 3, onClick = { selectedIndex = 3 }) {
-                    Text(text = "TECH")
-                }
-                Tab(selected = selectedIndex == 4, onClick = { selectedIndex = 4 }) {
-                    Text(text = "BEST FEED")
-                }
-            }
+    }
+}
+
+
+@Composable
+fun ListLazyColumn(categoryItems: LazyPagingItems<Category>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp)
+    ) {
+        items(categoryItems.itemCount) { index ->
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+//                            .data(bannerState.data?.get(page)?.imageUrl)
+                    .data(categoryItems[index]?.imageUrl)
+                    .crossfade(true).build(),
+                contentScale = ContentScale.FillWidth,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+            )
         }
 
     }
-
 }
