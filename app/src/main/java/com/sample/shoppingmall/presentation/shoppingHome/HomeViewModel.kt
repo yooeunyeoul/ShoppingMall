896c9caba951cd582fe5511141c916779cfe2f0f
youeuneyol <ey.yoo@fanmaum.com>
2023-05-23 13:00:51 +0900

    add: Feed List를 가져오기 위한 usecase, stateflow 로직 추가

--- app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeViewModel.kt
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeViewModel.kt
@@ -6,6 +6,7 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.sample.domain.usecase.GetBannerListUseCase
 import com.sample.domain.usecase.GetCategoryListUseCase
+import com.sample.domain.usecase.GetFeedListUseCase
 import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,6 +20,7 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     bannerUseCase: GetBannerListUseCase,
     categoryUseCase: GetCategoryListUseCase,
+    feedUseCase: GetFeedListUseCase,
 
 ) : ViewModel() {
 
@@ -51,6 +53,13 @@ class HomeViewModel @Inject constructor(
 
     val homeList =
         categoryUseCase(categoryType = CategoryType.HOME).flowOn(Dispatchers.IO).cachedIn(viewModelScope).stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PagingData.empty()
+        )
+
+    val feedList =
+        feedUseCase().flowOn(Dispatchers.IO).cachedIn(viewModelScope).stateIn(
             viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = PagingData.empty()
