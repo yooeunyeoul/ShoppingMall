e28a6c55f0c26b931ef23da2369cf1752ae81b46
youeuneyol <ey.yoo@fanmaum.com>
2023-05-23 21:09:12 +0900

    edit: Category Default 값 설정
    - Preview 작업 원활하게 하기위해 설정

--- core/domain/src/main/java/com/sample/domain/model/Category.kt
+++ core/domain/src/main/java/com/sample/domain/model/Category.kt
@@ -1,12 +1,13 @@
 package com.sample.domain.model
 
 data class Category(
-    val brandName: String,
-    val heartCount: Int,
-    val imageUrl: String,
-    val itemName: String,
-    val itemNo: Int,
-    val reviewAveragePoint: Double,
-    val reviewCount: Int,
-    val saleInfo: SaleInfo
+    val brandName: String="",
+    val heartCount: Int=0,
+    val imageUrl: String="",
+    val itemName: String="",
+    val itemNo: Int=0,
+    val reviewAveragePoint: Double= 0.0,
+    val reviewCount: Int=0,
+    val saleInfo: SaleInfo = SaleInfo(),
+    var isFavorite: Boolean = false
 )
