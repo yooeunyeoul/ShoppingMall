98002bb1e4f5447d8e4f2a77bb1114a03bcbb0bc
youeuneyol <ey.yoo@fanmaum.com>
2023-05-24 11:36:23 +0900

    add: 네트워크 에러, 익셉션 대응 로직 추가
    - 네트워크 에러, 익센셥 에러가 발생해도 리스트가 사용자에게 노출되도록 적용

--- core/data/src/main/java/com/sample/data/repository/datasourceImpl/ShoppingMallRemoteDataSourceImpl.kt
+++ core/data/src/main/java/com/sample/data/repository/datasourceImpl/ShoppingMallRemoteDataSourceImpl.kt
@@ -3,14 +3,14 @@
 package com.sample.data.repository.datasourceImpl
 
 import androidx.paging.ExperimentalPagingApi
-import androidx.paging.map
-import com.sample.data.mappers.toCategory
-import com.sample.localdata.local.CategoryEntity
-import kotlinx.coroutines.flow.map
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.map
+import androidx.room.withTransaction
 import com.sample.data.mappers.toBanner
+import com.sample.data.mappers.toBannerEntity
+import com.sample.data.mappers.toCategory
 import com.sample.data.mappers.toFeed
 import com.sample.data.repository.datasource.ShoppingMallRemoteDataSource
 import com.sample.data_paging.CategoryListRemoteMediator
@@ -21,6 +21,8 @@ import com.sample.domain.model.Feed
 import com.sample.domain.response.NetworkResult
 import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
+import com.sample.localdata.local.BannerEntity
+import com.sample.localdata.local.CategoryEntity
 import com.sample.localdata.local.FeedEntity
 import com.sample.localdata.local.ShoppingMallDatabase
 import com.sample.network.remote.ShoppingApi
@@ -28,6 +30,7 @@ import com.sample.network.response.toNetworkResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 @OptIn(ExperimentalPagingApi::class)
 class ShoppingMallRemoteDataSourceImpl(
@@ -39,22 +42,22 @@ class ShoppingMallRemoteDataSourceImpl(
             try {
                 when (val networkResponse = api.getBannerList().toNetworkResult()) {
                     is NetworkResult.Success -> {
-                        emit(Resource.Success(data = networkResponse.data.map { it.toBanner() }))
+                        var bannerEntityList = listOf<BannerEntity>()
+                        db.withTransaction {
+                            db.bannerDao().clearAll()
+                            db.bannerDao()
+                                .upsertAll(networkResponse.data.map { bannerDto -> bannerDto.toBannerEntity() })
+                            bannerEntityList = db.bannerDao().getBannerList().first()
+                        }
+                        emit(Resource.Success(data = bannerEntityList.map { bannerEntity -> bannerEntity.toBanner() }))
                     }
 
                     is NetworkResult.Fail -> {
-                        val bannerEntityList = db.imageDao().getBannerList().first()
-                        val bannerList = bannerEntityList.map { it.toBanner() }
-                        emit(
-                            Resource.Error(
-                                message = networkResponse.message ?: "",
-                                data = bannerList
-                            )
-                        )
+                        throw Exception(networkResponse.message)
                     }
                 }
             } catch (e: Exception) {
-                val bannerEntityList = db.imageDao().getBannerList().first()
+                val bannerEntityList = db.bannerDao().getBannerList().first()
                 val bannerList = bannerEntityList.map { it.toBanner() }
                 emit(Resource.Error(message = e.message ?: "", data = bannerList))
             }
@@ -63,7 +66,7 @@ class ShoppingMallRemoteDataSourceImpl(
 
     @OptIn(ExperimentalPagingApi::class)
     override fun getCategoryList(categoryType: CategoryType): Flow<PagingData<Category>> {
-        val pagingSourceFactory = { db.imageDao().pagingSource() }
+        val pagingSourceFactory = { db.categoryDao().pagingSource() }
         return Pager(
             config = PagingConfig(pageSize = 10),
             remoteMediator = CategoryListRemoteMediator(
