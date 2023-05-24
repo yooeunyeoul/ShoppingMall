3779503478e683a2ae1e6b518943e56e7ba76098
youeuneyol <ey.yoo@fanmaum.com>
2023-05-24 11:29:49 +0900

    edit: 네이밍 변경 및 쿼리 수정
    - ImageDao -> BannerDao 로 변경
    - 참조하는 entity 수정

--- core/data-localdata/src/main/java/com/sample/localdata/local/BannerDao.kt
+++ core/data-localdata/src/main/java/com/sample/localdata/local/BannerDao.kt
@@ -1,23 +1,18 @@
 package com.sample.localdata.local
 
-import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface ImageDao {
+interface BannerDao {
 
     @Upsert
-    suspend fun upsertAll(images: List<CategoryEntity>)
-
-    @Query("SELECT * FROM category_entity")
-    fun pagingSource(): PagingSource<Int, CategoryEntity>
+    suspend fun upsertAll(images: List<BannerEntity>)
 
     @Query("DELETE FROM banner_entity")
     suspend fun clearAll()
-
 
     @Query("SELECT * FROM banner_entity")
     fun getBannerList(): Flow<List<BannerEntity>>
