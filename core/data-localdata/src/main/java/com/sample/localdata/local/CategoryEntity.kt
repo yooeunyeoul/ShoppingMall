becc0dc2c6a8de9108e3a5d1c920d6ff8ea9ebf4
youeuneyol <ey.yoo@fanmaum.com>
2023-05-25 12:13:11 +0900

    edit: 카테고리 entity 프로퍼티 추가
    - isFavorite 좋아요 상태를 위해 추가
    - categoryType 카테고리 타입별로 db 쿼리를 조회하기 위함

--- core/data-localdata/src/main/java/com/sample/localdata/local/CategoryEntity.kt
+++ core/data-localdata/src/main/java/com/sample/localdata/local/CategoryEntity.kt
@@ -2,6 +2,7 @@ package com.sample.localdata.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.sample.domain.util.CategoryType
 import java.io.Serializable
 
 @Entity(tableName = "category_entity")
@@ -13,6 +14,9 @@ data class CategoryEntity(
     val imageUrl: String,
     val itemName: String,
     val reviewAveragePoint: Double,
-    val reviewCount: Int,
+    val reviewCount : Int ,
+    val isFavorite: Boolean,
+    val categoryType: CategoryType
+
 ) : Serializable
 
