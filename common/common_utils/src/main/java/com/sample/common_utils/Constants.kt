cbe94e120d68a5e6831a8130c7b8918601f73d16
youeuneyol <ey.yoo@fanmaum.com>
2023-05-25 23:56:40 +0900

    add: PreferencesKeys, Constats 추가
    - datastore 에 사용할 Preferences 추가
    - tab index를 저장할 목적으로 SAVED_TAB_INDEX 추가

--- common/common_utils/src/main/java/com/sample/common_utils/Constants.kt
+++ common/common_utils/src/main/java/com/sample/common_utils/Constants.kt
@@ -1,6 +1,11 @@
 package com.sample.common_utils
 
+import androidx.datastore.preferences.core.intPreferencesKey
+
 object Constants {
+    const val SHOPPING_MALL_PREFERENCES = "shopping_mall_preferences"
 
-
+}
+object PreferencesKeys {
+    val SAVED_TAB_INDEX = intPreferencesKey("tab_index")
 }
