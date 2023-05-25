bb858741d9f710e1dde68a89a074cf759d2e1d78
youeuneyol <ey.yoo@fanmaum.com>
2023-05-25 12:28:06 +0900

    add: feed 페이징 관리를 위한 dao, entity 추가

---
+++ core/data-localdata/src/main/java/com/sample/localdata/local/FeedRemoteKeysDao.kt
@@ -0,0 +1,20 @@
+package com.sample.localdata.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sample.domain.util.CategoryType
+
+@Dao
+interface FeedRemoteKeysDao {
+
+    @Query("SELECT * FROM feed_remote_keys_entity ORDER BY nextPage DESC LIMIT 1")
+    suspend fun getImageRemoteKey(): FeedRemoteKeysEntity?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addAllImageRemoteKeys(imageRemoteKeys: List<FeedRemoteKeysEntity>)
+
+    @Query("DELETE FROM feed_remote_keys_entity")
+    suspend fun deleteAllImageRemoteKeys()
+}
--- core/data-localdata/src/main/java/com/sample/localdata/local/FeedRemoteKeysEntity.kt
+++ core/data-localdata/src/main/java/com/sample/localdata/local/FeedRemoteKeysEntity.kt
@@ -2,12 +2,13 @@ package com.sample.localdata.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.sample.domain.util.CategoryType
 
 
-@Entity(tableName = "image_remote_keys_entity")
-data class ImageRemoteKeysEntity(
+@Entity(tableName = "feed_remote_keys_entity")
+data class FeedRemoteKeysEntity(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
     val prevPage: Int?,
-    val nextPage: Int?,
+    val nextPage: Int?
 )
