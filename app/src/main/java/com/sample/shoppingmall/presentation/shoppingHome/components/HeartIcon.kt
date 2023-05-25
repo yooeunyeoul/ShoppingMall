556919283018f8a101ca1e136d3edf74e30b025d
youeuneyol <ey.yoo@fanmaum.com>
2023-05-25 12:42:34 +0900

    edit: IconButton 추가
    - 퍼지는 애니메이션 추가를 위해 설정

--- app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/components/HeartIcon.kt
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/components/HeartIcon.kt
@@ -2,6 +2,7 @@ package com.sample.shoppingmall.presentation.shoppingHome.components
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -12,21 +13,28 @@ import com.sample.domain.model.Category
 import com.sample.shoppingmall.R
 
 @Composable
-fun HeartIcon(modifier: Modifier = Modifier, category: Category?) {
-    Icon(
-        painter = painterResource(
-            id = if (category?.isFavorite == true) R.drawable.icon_heartfilled
-            else R.drawable.icon_heart
-        ),
-        contentDescription = null,
-        modifier = modifier
-            .size(20.dp),
-        tint = if (category?.isFavorite == true) Color.Red else Color.Gray
-    )
+fun HeartIcon(
+    modifier: Modifier = Modifier,
+    category: Category?,
+    onItemClick: (Category) -> Unit
+) {
+
+    IconButton(onClick = { category?.let { onItemClick(it) } }) {
+        Icon(
+            painter = painterResource(
+                id = if (category?.isFavorite == true) R.drawable.icon_heartfilled
+                else R.drawable.icon_heart
+            ),
+            contentDescription = null,
+            modifier = modifier
+                .size(20.dp),
+            tint = if (category?.isFavorite == true) Color.Red else Color.Gray
+        )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewHeartIcon() {
-    HeartIcon(category = Category(isFavorite = true))
+    HeartIcon(category = Category(isFavorite = true), onItemClick = {})
 }
