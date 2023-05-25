ded352c0eb7d5ff4c3c0fae3c3d98f41a21f1669
youeuneyol <ey.yoo@fanmaum.com>
2023-05-25 12:14:22 +0900

    add: Home UI EVENTS 추가
    - ui Event 들을 위한 wrapper class 추가

---
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/ShoppingHomeEvents.kt
@@ -0,0 +1,8 @@
+package com.sample.shoppingmall.presentation.shoppingHome
+
+import com.sample.domain.model.Category
+
+sealed class ShoppingHomeEvents {
+    data class AddFavorite(val category: Category) : ShoppingHomeEvents()
+    data class DeleteFavorite(val category: Category) : ShoppingHomeEvents()
+}
