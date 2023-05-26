f0095c5a053521ae024a88a6868e26e004161595
youeuneyol <ey.yoo@fanmaum.com>
2023-05-26 14:26:37 +0900

    edit: tab 저장하는 로직 수정
    - 기존 viewmodel 에서 비지니스 로직을 수행하는 방식에서 datalayer 로 변경함 -> 코드의 일관성을 위해 그리고 테스트 코드를 작성하기 위해 변경

--- app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeViewModel.kt
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/HomeViewModel.kt
@@ -1,51 +1,58 @@
 package com.sample.shoppingmall.presentation.shoppingHome
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.PagerState
-import com.sample.common_utils.PreferencesKeys
 import com.sample.domain.usecase.CategoryUpdateFavoriteCase
 import com.sample.domain.usecase.GetBannerListUseCase
 import com.sample.domain.usecase.GetCategoryListUseCase
 import com.sample.domain.usecase.GetFeedListUseCase
+import com.sample.domain.usecase.GetSavedTabIndexUseCase
+import com.sample.domain.usecase.SaveTabIndexUseCase
 import com.sample.domain.util.CategoryType
 import com.sample.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @OptIn(ExperimentalPagerApi::class)
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    getBannerListUseCase: GetBannerListUseCase,
-    getCategoryListUseCase: GetCategoryListUseCase,
+    private val getBannerListUseCase: GetBannerListUseCase,
+    private val getCategoryListUseCase: GetCategoryListUseCase,
     private val categoryUpdateUseCase: CategoryUpdateFavoriteCase,
-    getFeedListUseCase: GetFeedListUseCase,
-    private val dataStore: DataStore<Preferences>
+    private val getFeedListUseCase: GetFeedListUseCase,
+    private val getSavedTabIndexUseCase: GetSavedTabIndexUseCase,
+    private val saveTabIndexUseCase: SaveTabIndexUseCase
 
 ) : ViewModel() {
 
 
-    var pagerState: PagerState = PagerState()
+    private val _pagerState = MutableStateFlow(PagerState())
+    val pagerState = _pagerState.asStateFlow()
 
     init {
+        getSavedTabIndex()
+    }
+
+    private fun getSavedTabIndex() {
         viewModelScope.launch {
-            pagerState = dataStore.data.map { preferences ->
-                val tabNumber = preferences[PreferencesKeys.SAVED_TAB_INDEX] ?: 0
-                PagerState(currentPage = tabNumber)
-            }.first()
+            getSavedTabIndexUseCase().map { index ->
+                PagerState(index)
+            }.collect{pagerState->
+                _pagerState.update { pagerState }
+            }
         }
     }
 
@@ -101,14 +108,10 @@ class HomeViewModel @Inject constructor(
                 is ShoppingHomeEvents.DeleteFavorite -> {
                     categoryUpdateUseCase(events.category.copy(isFavorite = false))
                 }
-            }
-        }
-    }
 
-    fun saveTabIndex() {
-        viewModelScope.launch {
-            dataStore.edit { preferences ->
-                preferences[PreferencesKeys.SAVED_TAB_INDEX] = pagerState.currentPage
+                is ShoppingHomeEvents.SaveTabIndex -> {
+                    saveTabIndexUseCase(pagerState.value.currentPage)
+                }
             }
         }
     }
