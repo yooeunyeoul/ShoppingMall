7b159ccc02e71a59d5dee3fd9675f536321db973
youeuneyol <ey.yoo@fanmaum.com>
2023-05-26 12:12:51 +0900

    add: 상수값 공통처리 추가

--- common/common_utils/src/main/java/com/sample/common_utils/Constants.kt
+++ common/common_utils/src/main/java/com/sample/common_utils/Constants.kt
@@ -4,8 +4,12 @@ import androidx.datastore.preferences.core.intPreferencesKey
 
 object Constants {
     const val SHOPPING_MALL_PREFERENCES = "shopping_mall_preferences"
+    const val CATEGORY_PER_PAGE_SIZE = 20
+    const val FEED_PER_PAGE_SIZE = 10
+    const val PRE_LOAD_SIZE = 10
 
 }
 object PreferencesKeys {
     val SAVED_TAB_INDEX = intPreferencesKey("tab_index")
+
 }
