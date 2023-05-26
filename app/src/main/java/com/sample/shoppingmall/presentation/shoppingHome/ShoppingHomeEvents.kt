0d1744a6a8e004bfe88fe089398015392bcfdc40
youeuneyol <ey.yoo@fanmaum.com>
2023-05-26 14:23:32 +0900

    add: tab 저장하는 이벤트 공통처리 추가

--- app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/ShoppingHomeEvents.kt
+++ app/src/main/java/com/sample/shoppingmall/presentation/shoppingHome/ShoppingHomeEvents.kt
@@ -5,4 +5,5 @@ import com.sample.domain.model.Category
 sealed class ShoppingHomeEvents {
     data class AddFavorite(val category: Category) : ShoppingHomeEvents()
     data class DeleteFavorite(val category: Category) : ShoppingHomeEvents()
+    object SaveTabIndex : ShoppingHomeEvents()
 }
