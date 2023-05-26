8ab80642e19e528dfb8b04dbab30239212679de1
youeuneyol <ey.yoo@fanmaum.com>
2023-05-27 00:35:43 +0900

    edit: error_code 기본값 추가

--- core/data-network/src/main/java/com/sample/network/base/CommonPagingListResponse.kt
+++ core/data-network/src/main/java/com/sample/network/base/CommonPagingListResponse.kt
@@ -1,7 +1,7 @@
 package com.sample.network.base
 
 data class CommonPagingListResponse<T>(
-    val error_code: Int?,
+    val error_code: Int?=null,
     val list: List<T>,
     val result: String,
     val total_count: Int
