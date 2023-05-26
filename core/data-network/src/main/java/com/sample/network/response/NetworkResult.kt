bf9552bdf0aa3c8a6072c82ade7ec3518f2473dd
youeuneyol <ey.yoo@fanmaum.com>
2023-05-27 00:35:20 +0900

    edit: NetworkResult.kt 하드코딩 -> Constants 로 변경

--- core/data-network/src/main/java/com/sample/network/response/NetworkResult.kt
+++ core/data-network/src/main/java/com/sample/network/response/NetworkResult.kt
@@ -1,19 +1,20 @@
 package com.sample.network.response
 
+import com.sample.common_utils.Constants
 import com.sample.domain.response.NetworkResult
 import com.sample.network.base.CommonListResponse
 import com.sample.network.base.CommonPagingListResponse
 
 fun <T> CommonListResponse<T>.toNetworkResult(): NetworkResult<List<T>> {
     return when (result) {
-        "SUCCESS" -> NetworkResult.Success(list)
+        Constants.API_SUCCESS -> NetworkResult.Success(list)
         else -> NetworkResult.Fail(data = null, message = "Network Error, ErrorCode : $error_code")
     }
 }
 
 fun <T> CommonPagingListResponse<T>.toNetworkResult(): NetworkResult<List<T>> {
     return when (result) {
-        "SUCCESS" -> NetworkResult.Success(list)
+        Constants.API_SUCCESS -> NetworkResult.Success(list)
         else -> NetworkResult.Fail(data = null, message = "Network Error, ErrorCode : $error_code")
     }
 }
